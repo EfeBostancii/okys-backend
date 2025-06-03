56c780b71321e80a14e9bf3d8a9ddf76767d33bf
EfeBostancii <46efebostancii@gmail.com>
2025-06-04 01:16:30 +0300

    Update OkysapiApplication.java

--- src/main/java/com/efe/okys/okysapi/OkysapiApplication.java
+++ src/main/java/com/efe/okys/okysapi/OkysapiApplication.java
@@ -3,8 +3,6 @@ package com.efe.okys.okysapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /*
  * Bu sınıf, Spring Boot uygulamasının ana giriş noktasıdır.
@@ -16,15 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @EnableAsync // Bunun sayesinde @Async kullanılan metodlar ayrı thread'de çalışır
 @SpringBootApplication
-@RestController
 public class OkysapiApplication {
 
 	public static void main(String[] args) {
 		// Uygulama buradan başlatılır
 		SpringApplication.run(OkysapiApplication.class, args);
-	}
-	@GetMapping("/")
-	public String home() {
-		return "Test: API Ayakta";
 	}
 }
