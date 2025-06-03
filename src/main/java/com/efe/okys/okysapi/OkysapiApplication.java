805df2d2b320d69c143b04ba4d5a06ae271b66fe
EfeBostancii <46efebostancii@gmail.com>
2025-06-04 00:51:40 +0300

    Update OkysapiApplication.java
    Açıklama İptal Edildi

--- src/main/java/com/efe/okys/okysapi/OkysapiApplication.java
+++ src/main/java/com/efe/okys/okysapi/OkysapiApplication.java
@@ -3,8 +3,6 @@ package com.efe.okys.okysapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /*
  * Bu sınıf, Spring Boot uygulamasının ana giriş noktasıdır.
@@ -12,21 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * - Spring Boot başlangıç yapısı (main metodu ile çalıştırma)
  * - @SpringBootApplication ile uygulama yapılandırması
  * - Multithread programlama için @EnableAsync kullanımı
- * - Basit bir root endpoint ile proje durumu mesajı sağlanması
  */
 
 @EnableAsync // Bunun sayesinde @Async kullanılan metodlar ayrı thread'de çalışır
 @SpringBootApplication
-@RestController // Bu sınıf aynı zamanda REST Controller olarak da görev yapar
 public class OkysapiApplication {
 
 	public static void main(String[] args) {
 		// Uygulama buradan başlatılır
 		SpringApplication.run(OkysapiApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String home() {
-		return "Test: API Calisiyor";
 	}
 }
